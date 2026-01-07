0797c491df4f3753a4deec8d6ebafad4d485dd08
Christian Gonzalez <chrisalmonte@protonmail.com>
2026-01-07 17:06:12 -0600

    Update logging.py

--- logging.py
+++ logging.py
@@ -2,17 +2,35 @@ import os
 from datetime import datetime
 
 class Log:
-    def __init__(self, path: str):
-        #Validar directorio de salida
+    def __init__(self, path: str, prefix: str="log_"):
+        #Vaidate path
         if not os.path.exists(path):
             try:
                 os.mkdir(path)
             except:
                 raise OSError("The directory could not be created.")
+        self.prefix = prefix
         self.path = path
+        self.messages = []
+        self.properties = {}
+    
+    def log_message(self, message: str):
+        self.messages.append(message)
+    
+    def log_property(self, key: str, value):
+        self.properties[key] = value
 
-    def save(self, filename: str):
-        path = "%s%s_%s.txt" % (self.path, filename, datetime.now().strftime("%Y_%m_%d-%H%M%S"))        
-        with open(path, 'w', encoding="utf-8") as file:
+    def log_properties(self, properties: dict):
+        for key, value in properties.items():
+            self.properties[key] = value
+
+    def save(self):
+        file_path = f"{self.path}/{self.prefix}_{datetime.now().strftime('%Y_%m_%d-%H%M%S')}.txt"       
+        with open(file_path, 'w', encoding="utf-8") as file:
             file.write(datetime.now().strftime("%b %d %Y - %H:%M:%S") + "\n")
+            file.write("History" + "\n")            
+            file.write("\n".join(self.messages) + "\n\n")
+            file.write("Properties" + "\n")
+            for key, value in self.properties.items():
+                file.write(f"{key}: {value}\n")
     
