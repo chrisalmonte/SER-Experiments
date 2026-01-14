e2d088ec05071f005460745467ee9f0eb22c7a07
Christian Gonzalez <chrisalmonte@protonmail.com>
2026-01-14 11:48:22 -0600

    Update logging.py

--- logging.py
+++ logging.py
@@ -12,25 +12,26 @@ class Log:
         self.prefix = prefix
         self.path = path
         self.messages = []
-        self.properties = {}
+        self.properties = []
     
     def log_message(self, message: str):
-        self.messages.append(message)
+        self.messages.append(f"{datetime.now().strftime('%Y_%m_%d-%H:%M:%S')}: {message}")
     
     def log_property(self, key: str, value):
-        self.properties[key] = value
+        self.properties.append(f"{key}: {value}")
 
-    def log_properties(self, properties: dict):
+    def log_properties(self, set_name:str ,properties: dict):
+        self.properties.append('\n' + set_name)
         for key, value in properties.items():
-            self.properties[key] = value
+            self.properties.append(f"{key}: {value}")
 
     def save(self):
         file_path = f"{self.path}/{self.prefix}_{datetime.now().strftime('%Y_%m_%d-%H%M%S')}.txt"       
         with open(file_path, 'w', encoding="utf-8") as file:
+            file.write("Log saved on: ")
             file.write(datetime.now().strftime("%b %d %Y - %H:%M:%S") + "\n")
-            file.write("History" + "\n")            
+            file.write("-----Properties-----\n")
+            file.write("\n".join(self.properties) + "\n\n")
+            file.write("-----History-----\n")
             file.write("\n".join(self.messages) + "\n\n")
-            file.write("Properties" + "\n")
-            for key, value in self.properties.items():
-                file.write(f"{key}: {value}\n")
     
