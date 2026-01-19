4b2ab01b52ab82e7f70ba985747a60aee5b27d99
Christian Gonzalez <chrisalmonte@protonmail.com>
2026-01-19 13:15:07 -0600

    Change Logger Nomenclature

--- cLogger.py
+++ cLogger.py
@@ -13,6 +13,7 @@ class Log:
         self.path = path
         self.messages = []
         self.properties = []
+        self.epoch = []
     
     def log_message(self, message: str):
         self.messages.append(f"{datetime.now().strftime('%Y_%m_%d-%H:%M:%S')}: {message}")
@@ -25,13 +26,23 @@ class Log:
         for key, value in properties.items():
             self.properties.append(f"{key}: {value}")
 
+    def log_epoch(self, epoch, properties: dict):
+        self.properties.append('\nEpoch num. ' + epoch)
+        for key, value in properties.items():
+            self.properties.append(f"{key}: {value}")
+
     def save(self):
         file_path = f"{self.path}/{self.prefix}_{datetime.now().strftime('%Y_%m_%d-%H%M%S')}.txt"       
         with open(file_path, 'w', encoding="utf-8") as file:
             file.write("Log saved on: ")
             file.write(datetime.now().strftime("%b %d %Y - %H:%M:%S") + "\n")
-            file.write("-----Properties-----\n")
-            file.write("\n".join(self.properties) + "\n\n")
-            file.write("-----History-----\n")
-            file.write("\n".join(self.messages) + "\n\n")
+            if self.properties:
+                file.write("-----Properties-----\n")
+                file.write("\n".join(self.properties) + "\n\n")
+            if self.messages:
+                file.write("-----History-----\n")
+                file.write("\n".join(self.messages) + "\n\n")
+            if self.epoch:
+                file.write("-----Training-----\n")
+                file.write("\n".join(self.messages) + "\n\n")
     
