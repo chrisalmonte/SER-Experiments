e8d49a967272d8e9c1234717962ee0e92abc16c3
Christian Gonzalez <chrisalmonte@protonmail.com>
2026-01-19 16:39:35 -0600

    Update Logger

--- cLogger.py
+++ cLogger.py
@@ -1,4 +1,5 @@
 import os
+import inspect
 from datetime import datetime
 
 class Log:
@@ -17,32 +18,46 @@ class Log:
     
     def log_message(self, message: str):
         self.messages.append(f"{datetime.now().strftime('%Y_%m_%d-%H:%M:%S')}: {message}")
+        return message
     
     def log_property(self, key: str, value):
-        self.properties.append(f"{key}: {value}")
+        property = f"{key}: {self.str_property(value)}"
+        self.properties.append(property)
+        return property
 
     def log_properties(self, set_name:str ,properties: dict):
-        self.properties.append('\n' + set_name)
+        self.properties.append("\n" + set_name + ":\n")
         for key, value in properties.items():
-            self.properties.append(f"{key}: {value}")
+            self.properties.append(f"{key}: {self.str_property(value)}")
+        self.properties.append('\n')
 
     def log_epoch(self, epoch, properties: dict):
-        self.properties.append('\nEpoch num. ' + epoch)
+        self.epoch.append('\nEpoch num. ' + str(epoch))
         for key, value in properties.items():
-            self.properties.append(f"{key}: {value}")
+            self.epoch.append(f"{key}: {value}")
+        self.epoch.append('\n\n')
 
     def save(self):
         file_path = f"{self.path}/{self.prefix}_{datetime.now().strftime('%Y_%m_%d-%H%M%S')}.txt"       
         with open(file_path, 'w', encoding="utf-8") as file:
             file.write("Log saved on: ")
-            file.write(datetime.now().strftime("%b %d %Y - %H:%M:%S") + "\n")
+            file.write(datetime.now().strftime("%b %d %Y - %H:%M:%S") + "\n\n")
             if self.properties:
-                file.write("-----Properties-----\n")
+                file.write("-----Properties-----\n\n")
                 file.write("\n".join(self.properties) + "\n\n")
             if self.messages:
-                file.write("-----History-----\n")
+                file.write("-----History-----\n\n")
                 file.write("\n".join(self.messages) + "\n\n")
             if self.epoch:
-                file.write("-----Training-----\n")
-                file.write("\n".join(self.messages) + "\n\n")
+                file.write("-----Training-----\n\n")
+                file.write("\n".join(self.epoch) + "\n\n")
+    
+    @staticmethod
+    def str_property(property):
+        if inspect.isfunction(property) or inspect.ismethod(property):
+            return property.__name__
+        if callable(property):
+            return property.__class__.__name__
+        return str(property)
+
     
