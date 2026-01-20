023ef9181a42861ea18fd21efed8309548325edb
Christian Gonzalez <chrisalmonte@protonmail.com>
2026-01-20 12:01:52 -0600

    Update cLogger

--- cLogger.py
+++ cLogger.py
@@ -1,4 +1,5 @@
 import os
+import pickle
 import inspect
 from datetime import datetime
 
@@ -15,6 +16,8 @@ class Log:
         self.messages = []
         self.properties = []
         self.epoch = []
+        self.timings = []
+        self.tracked_time_start = None
         self.last_save = None
     
     def log_message(self, message: str, show: bool=False):
@@ -42,12 +45,17 @@ class Log:
             self.epoch.append(f"{key}: {value}")
         self.properties.append(' ')
 
-    def save(self, overwrite: bool=False):
+    def save(self, overwrite: bool=True):
         if overwrite and self.last_save:
             file_path = self.last_save
         else:
-            file_path = f"{self.path}/{self.prefix}_{datetime.now().strftime('%Y_%m_%d-%H%M%S')}.txt"       
+            file_path = f"{self.path}/{self.prefix}_{datetime.now().strftime('%Y_%m_%d-%H%M%S')}.pkl"       
             self.last_save = file_path
+        with open(file_path, 'wb') as file:
+            pickle.dump(self, file)
+
+    def save_txt(self):
+        file_path = f"{self.path}/{self.prefix}_{datetime.now().strftime('%Y_%m_%d-%H%M%S')}.txt"
         with open(file_path, 'w', encoding="utf-8") as file:
             file.write("Log saved on: ")
             file.write(datetime.now().strftime("%b %d %Y - %H:%M:%S") + "\n\n")
@@ -60,6 +68,27 @@ class Log:
             if self.epoch:
                 file.write("-----Training-----\n\n")
                 file.write("\n".join(self.epoch) + "\n\n")
+            if self.timings:
+                file.write("-----Timings-----\n\n")
+                file.write("\n".join(self.timings) + "\n\n")
+        
+    def track_time(self, track: bool, message: str=""):
+        if not track:
+            self.tracked_time_start = None
+        else:
+            if self.tracked_time_start:
+                self.log_message("Time tracking reset.", show=True)
+            self.tracked_time_start = datetime.now()
+            self.log_message(message if message else "Time tracking started.", show=True)            
+    
+    def log_elapsed_time(self, message: str="", reset_timer: bool=False):        
+        if not self.tracked_time_start:
+            raise ValueError("Time tracking has not been started. Call track_time() first.")
+        else:
+            elapsed_time = datetime.now() - self.tracked_time_start
+            self.timings.append(f"{message if message else "Elapsed time"}: {str(elapsed_time)}")
+            if reset_timer:
+                self.tracked_time_start = datetime.now()
     
     @staticmethod
     def str_property(property):
