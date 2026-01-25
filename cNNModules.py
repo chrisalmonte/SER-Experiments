02d8ca58557694c1f12a80bdcab2cd043bf0964f
Christian Gonzalez <chrisalmonte@protonmail.com>
2026-01-25 00:32:07 -0600

    Add baseline model

--- cNNModules.py
+++ cNNModules.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn
 
-class MaskedAvgPool1d(nn.Module):
+class MaskedAvgPool1D(nn.Module):
     def __init__(self):
         super().__init__()
         
@@ -12,11 +12,10 @@ class MaskedAvgPool1d(nn.Module):
         mask = mask.unsqueeze(1)
         
         # Mask input
-        x_masked = x * mask        
-
+        x_masked = x * mask
         sum_features = x_masked.sum(dim=2)
-        lengths = lengths.unsqueeze(1).float().to(x.device)
-        lengths = torch.clamp(lengths, min=1.0)
+
+        valid_counts = mask.sum(dim=2).clamp_min(1).float()
         
-        return sum_features / lengths
+        return sum_features / valid_counts
     
