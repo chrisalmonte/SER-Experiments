62054a0a5dc7cf1cc33fc92a9489df2a63187bd8
Christian Gonzalez <chrisalmonte@protonmail.com>
2026-01-28 19:25:33 -0600

    Add undersampling dev set code

--- scripts/msp_undersampling.py
+++ scripts/msp_undersampling.py
@@ -19,24 +19,39 @@ df = pd.read_csv('C:/Datasets/MSP-PODCAST-Publish-2.0/Labels/labels_consensus.cs
 df_filtered = df[~df['FileName'].isin(missing_files)]
 
 #Remove test and dev samples
-df_filtered = df_filtered[df_filtered['Split_Set'] == 'Train']
-#Keep only name and targets
-df_filtered = df_filtered.drop(columns=['Split_Set', 'Gender', 'SpkrID'])
+#df_filtered = df_filtered[df_filtered['Split_Set'] == 'Train']
+##Keep only name and targets
+#df_filtered = df_filtered.drop(columns=['Split_Set', 'Gender', 'SpkrID'])
+#
+##Define classes and desired samples per class
+#classes = df_filtered['EmoClass'].unique()
+#samples_per_class = 500
+#undersampled_dfs = []
+#for emo_class in classes:
+#    class_df = df_filtered[df_filtered['EmoClass'] == emo_class]
+#    if len(class_df) > samples_per_class:
+#        sampled_df = class_df.sample(n=samples_per_class, random_state=42)
+#    else:
+#        sampled_df = class_df
+#    undersampled_dfs.append(sampled_df)
+#undersampled_df = pd.concat(undersampled_dfs)
+#
+##Drop unneeded columns
+#undersampled_df = undersampled_df.drop(columns=['EmoClass'])
+#
+#undersampled_df.to_csv(results_path, index=False)
 
-#Define classes and desired samples per class
-classes = df_filtered['EmoClass'].unique()
-samples_per_class = 500
-undersampled_dfs = []
-for emo_class in classes:
-    class_df = df_filtered[df_filtered['EmoClass'] == emo_class]
-    if len(class_df) > samples_per_class:
-        sampled_df = class_df.sample(n=samples_per_class, random_state=42)
-    else:
-        sampled_df = class_df
-    undersampled_dfs.append(sampled_df)
-undersampled_df = pd.concat(undersampled_dfs)
+#Select x random samples from test set for evaluation
+test_df = df_filtered[df_filtered['Split_Set'] == 'Train']
+test_df = test_df.drop(columns=['Split_Set', 'Gender', 'SpkrID'])
+total_samples = 1500
+if len(test_df) > total_samples:
+    test_sampled_df = test_df.sample(n=total_samples, random_state=42)
+else:
+    test_sampled_df = test_df
+test_results_path = os.path.join(script_dir, '..', 'data', 'labels_dev_msp_consensus_undersampled.csv')
 
 #Drop unneeded columns
-undersampled_df = undersampled_df.drop(columns=['EmoClass'])
+test_sampled_df = test_sampled_df.drop(columns=['EmoClass'])
 
-undersampled_df.to_csv(results_path, index=False)
+test_sampled_df.to_csv(test_results_path, index=False)
