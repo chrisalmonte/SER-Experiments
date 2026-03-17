16e3968664bb0119d52ab0b4b5d2dfea4f55d7b1
chrisalmonte <chrisalmonte@protonmail.com>
2026-03-17 18:30:24 +0900

    Add embeddings training

--- scripts/emotion2vec_extract_embeddings.py
+++ scripts/emotion2vec_extract_embeddings.py
@@ -4,7 +4,6 @@ import pandas as pd
 from cAudiotools import Utils
 from funasr import AutoModel
 from pathlib import Path
-from tqdm import tqdm
 
 if __name__ == "__main__":
 
@@ -14,16 +13,15 @@ if __name__ == "__main__":
         hub="hf", 
     )
 
-    labels = pd.read_csv(r"C:\Datasets\MSP-PODCAST-Publish-2.0\Labels\custom\divided_labels_consensus.csv")
-    master_dir = r"C:\Datasets\_compiled"
-    labels['FullPath'] = labels.iloc[:, 0].str.cat(labels.iloc[:, 1], sep='\\', na_rep='')
-    labels['FullPath'] = labels.iloc[:, 0].str.cat(labels.iloc[:, 1], sep='\\', na_rep='')
+    master_dir = "/home/imd-temp/datasets/"
+    labels = pd.read_csv("/home/imd-temp/datasets/msp-podcast-2_divided/labels/divided_labels_consensus.csv")
+    labels['FullPath'] = labels.iloc[:, 0].str.cat(labels.iloc[:, 1], sep='/', na_rep='')
     audio_paths = labels['FullPath'].tolist()
     
-    for path in tqdm(audio_paths):
+    for path in audio_paths:
         path = os.path.join(master_dir, path)
         file_name = Path(path).stem
         audio = Utils.load_as_np(path)
         rec_result = model.generate(audio[0], output_dir=None, granularity="utterance")
         embeddings = np.array(rec_result[0]['feats'])
-        np.save(f"output/processing/e2v_embeddings/{file_name}.npy", embeddings)
+        np.save(f"./output/processing/e2v_embeddings/{file_name}.npy", embeddings)
