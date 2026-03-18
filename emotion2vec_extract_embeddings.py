6ed1a8c1b17080be6c6ae8c5a65f32d2adc3e251
chrisalmonte <chrisalmonte@protonmail.com>
2026-03-18 12:44:55 +0900

    Update emotion2vec extraction script

--- emotion2vec_extract_embeddings.py
+++ emotion2vec_extract_embeddings.py
@@ -8,13 +8,14 @@ from pathlib import Path
 if __name__ == "__main__":
 
     model_id = "emotion2vec/emotion2vec_base"
+    granularity = "frame"
     model = AutoModel(
         model=model_id,
         hub="hf", 
     )
 
     master_dir = "/home/imd-temp/datasets/"
-    labels = pd.read_csv("/home/imd-temp/datasets/msp-podcast-2_divided/labels/divided_labels_consensus.csv")
+    labels = pd.read_csv("/home/imd-temp/datasets/ravdess/labels/ravdess_labels_speech.csv")
     labels['FullPath'] = labels.iloc[:, 0].str.cat(labels.iloc[:, 1], sep='/', na_rep='')
     audio_paths = labels['FullPath'].tolist()
     
@@ -22,6 +23,6 @@ if __name__ == "__main__":
         path = os.path.join(master_dir, path)
         file_name = Path(path).stem
         audio = Utils.load_as_np(path)
-        rec_result = model.generate(audio[0], output_dir=None, granularity="utterance")
+        rec_result = model.generate(audio[0], output_dir=None, granularity=granularity)
         embeddings = np.array(rec_result[0]['feats'])
-        np.save(f"./output/processing/e2v_embeddings/{file_name}.npy", embeddings)
+        np.save(f"output/processing/e2v_embeddings_{granularity}/{file_name}.npy", embeddings)
