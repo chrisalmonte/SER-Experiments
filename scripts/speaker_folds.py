68effcfaeb873bca5d6aa03103ef84afcbf712fa
Christian Gonzalez <chrisalmonte@protonmail.com>
2026-04-01 01:12:32 +0900

    Update folds script

--- scripts/speaker_folds.py
+++ scripts/speaker_folds.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.model_selection import StratifiedGroupKFold
 
 LABELS_PATH = "output/processing/custom_labels/ravdess/ravdess_labels_speech.csv"
+OUTPUT_PATH = "output/processing/custom_labels/ravdess/labels_speech_folds.csv"
 
 df = pd.read_csv(LABELS_PATH)
 df['SpkrID'] = df['SpkrID'].astype(int)
@@ -18,10 +19,4 @@ for i in range(1, 7):
     actors = sorted(fold_data['SpkrID'].unique())
     print(f"Fold {i} Actors: {actors}")
 
-df.to_csv("output/processing/custom_labels/ravdess/labels_w_folds.csv", index=False)
-
-
-
-# --- How to use it ---
-# master_df = assign_ravdess_folds("your_dataset.csv")
-# master_df.to_csv("ravdess_with_folds.csv", index=False)
+df.to_csv(OUTPUT_PATH, index=False)
